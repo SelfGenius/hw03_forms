0831b4d51a03a2bdaba3f5bee4af09d94ed726f3
Александр Гетманов <getmanov.wyrik@yandex.ru>
2022-04-18 18:53:00 +0300

    view mod

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -68,11 +68,11 @@ def post_create(request):
 
 @login_required
 def post_edit(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
+    post = get_object_or_404(Post.objects.select_related('author'), pk=post_id)
     form = PostForm(request.POST or None, instance=post)
     if request.user == post.author:
         if form.is_valid():
-            post.save()
+            form.save()
             return redirect('posts:post_detail', post_id=post_id)
 
         return render(request, 'posts/create_post.html',
